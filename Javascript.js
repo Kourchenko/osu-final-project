6bee8c868698d035d26c0bc2416f210412afac82
Kourchenko <dkourchenko@yahoo.com>
2018-05-16 09:20:20 -0700

    Fixed footer bug, defined flags in JS file

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,7 @@
 /* JS */
+
+// Modal hidden flags
+var DIV_USERNAME_HIDDEN = false; // 1st to load
+var DIV_DRAW_HIDDEN = true; // 2nd
+var DIV_LOADING_HIDDEN = true; // 3rd
+var DIV_MATCH_HIDDEN = true; // 4th 
