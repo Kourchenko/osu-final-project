f2b75eea626ee41b72c128184766bc02f548a1fb
Aaron Leenknecht <32150461+AaronLeenknecht@users.noreply.github.com>
2018-05-16 20:52:38 -0700

    Added jump to first page to see if it works

--- Javascript.js
+++ Javascript.js
@@ -5,3 +5,15 @@ var DIV_USERNAME_HIDDEN = false; // 1st to load
 var DIV_DRAW_HIDDEN = true; // 2nd
 var DIV_LOADING_HIDDEN = true; // 3rd
 var DIV_MATCH_HIDDEN = true; // 4th 
+
+
+var main = document.getElementsByClassName('main');
+var drawpic1 = document.getElementsByClassName('draw_pic');
+var clickconnect = document.getElementByID('clickconnect');
+
+function LeaveMain {
+    main.classList.add('hidden');
+    drawpic1.classList.remove('hidden');
+}
+
+clickconnect.addEventListener('click', LeaveMain);
