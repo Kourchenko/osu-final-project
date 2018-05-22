521009d2b517c69a4c1de8de5ce22c72cc8cbe0f
Kourchenko <dkourchenko@yahoo.com>
2018-05-21 18:13:14 -0700

    Updated server.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var DIV_USERNAME_HIDDEN = false; // 1st to load
 var DIV_DRAW_HIDDEN = true; // 2nd
 var DIV_LOADING_HIDDEN = true; // 3rd
-var DIV_MATCH_HIDDEN = true; // 4th 
+var DIV_MATCH_HIDDEN = true; // 4th
 
 
 
@@ -12,7 +12,7 @@ var DIV_MATCH_HIDDEN = true; // 4th
 
 
 /*
-			ELEMENT CONNECTORS          
+			ELEMENT CONNECTORS
 								AUTHOR: TEAM			*/
 var main = document.getElementsByClassName('main')[0];
 var drawpic1 = document.getElementsByClassName('draw_pic1')[0];
@@ -32,8 +32,8 @@ var matchfound_no = document.getElementsByClassName('matchfound_no')[0];
 
 
 /*
-			FLOW FUNCTIONS          
-								AUTHOR: Darius			*/											
+			FLOW FUNCTIONS
+								AUTHOR: Darius			*/
 function main_to_drawpic1() {
     main.classList.add('hidden');
     drawpic1.classList.remove('hidden');
@@ -80,10 +80,10 @@ function askchatmodal_to_chat() {
 
 
 /*
-			EVENT LISTENERS          
+			EVENT LISTENERS
 								AUTHOR: TEAM			*/
 main_connect.addEventListener('click', main_to_drawpic1);
-askchatmodal_yes.addEventListener('click', askchatmodal_to_chat);
-askchatmodal_no.addEventListener('click', askchatmodal_to_loading);
+askchatmodal_yes.addEventListener('click', askchatmodal_chat);
+askchatmodal_no.addEventListener('click', askchatmodal_loading);
 matchfound_yes.addEventListener('click', matchfound_to_drawpic2);
 matchfound_no.addEventListener('click', matchfound_to_loading);
--- server/server.js
+++ server/server.js
@@ -6,14 +6,6 @@
  * for a web application.
  */
 
-// Set up server code with expressJS
-var express = require('express');
-// Set up unique, anonymous user ID
-var cookieParser = require('cookie-parser');
-var http = require('http');
-// Easy form submissions, for web chat
-var bodyParser = require('body-parser');
-var postgreSQLDB = require('pg');
 
 // Error Codes
 var HTTP_CODE_OK = 200;
@@ -23,15 +15,15 @@ var HTTP_CODE_CLIENT_ERR = 400;
 var HTTP_CODE_NOT_FOUND = 404;
 var HTTP_CODE_SERVER_ERR = 500;
 
-// Application
-var app = express();
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+var app = require('express')();
+var fs = require('fs');
+var syncIndex = fs.readFileSync(__dirname+"/../index.html", "utf-8");
+var syncCSS = fs.readFileSync(__dirname+"/../style.css");
 
-var router = express.Router();
-router.use(function(req, res, next() {
-	next();
+var server = http.createServer(function(req, res) {
+	if (req.url === "/index.html") {
+		res.end(syncIndex);
+	} else if (req.url == "/style.css") {
+		res.end(syncCSS)
+	}
 });
