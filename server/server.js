62d481907dd7695ec4a21c0282543ea9860962a3
Kourchenko <dkourchenko@yahoo.com>
2018-05-21 21:49:10 -0700

    Added socket.io code

--- server/server.js
+++ server/server.js
@@ -14,16 +14,47 @@ var HTTP_CODE_NO_CONTENT = 204;
 var HTTP_CODE_CLIENT_ERR = 400;
 var HTTP_CODE_NOT_FOUND = 404;
 var HTTP_CODE_SERVER_ERR = 500;
+var users = 0;
 
+var http = require('http');
 var app = require('express')();
 var fs = require('fs');
 var syncIndex = fs.readFileSync(__dirname+"/../index.html", "utf-8");
-var syncCSS = fs.readFileSync(__dirname+"/../style.css");
+var syncCSS = fs.readFileSync(__dirname+"/../style.css", "utf-8");
+var syncIndexJS = fs.readFileSync(__dirname+"/../index.js", "utf-8");
 
 var server = http.createServer(function(req, res) {
 	if (req.url === "/index.html") {
 		res.end(syncIndex);
 	} else if (req.url == "/style.css") {
-		res.end(syncCSS)
+		res.end(syncCSS);
+	} else if (req.url == "/index.js") {
+		res.end(syncIndexJS)
+	} else {
+		// send content of file -- '/socket.io/socket.io.js'
+		fs.readFile("/../index.html", "utf-8", function(err, content) {
+			res.end(content);
+		});
 	}
 });
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.emit('message', "You are connected");
+	addUsers();
+	socket.on('new_username', function(username) {
+		socket.username = username;
+	});
+	// display number of users, should be unique users
+	socket.on('message', function(message) {
+		console.log("new message from: " + socket.username);
+	});
+});
+
+server.listen(8080);
+
+
+function addUsers() {
+	users+=1;
+}
