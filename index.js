fd6518fc9c8d71fe4892acf7884c57aa6f5084f9
dmoseley <38228404+dmoseley@users.noreply.github.com>
2018-05-21 18:48:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var DIV_USERNAME_HIDDEN = false; // 1st to load
 var DIV_DRAW_HIDDEN = true; // 2nd
 var DIV_LOADING_HIDDEN = true; // 3rd
-var DIV_MATCH_HIDDEN = true; // 4th
+var DIV_MATCH_HIDDEN = true; // 4th 
 
 
 
@@ -12,7 +12,7 @@ var DIV_MATCH_HIDDEN = true; // 4th
 
 
 /*
-			ELEMENT CONNECTORS
+			ELEMENT CONNECTORS          
 								AUTHOR: TEAM			*/
 var main = document.getElementsByClassName('main')[0];
 var drawpic1 = document.getElementsByClassName('draw_pic1')[0];
@@ -23,6 +23,7 @@ var askchatmodal = document.getElementsByClassName('askchatmodal')[0];
 var times_up = document.getElementsByClassName('times_up')[0];
 
 var timer_count1 = document.getElementsByClassName('timer1')[0];
+var timer_count2 = document.getElementsByClassName('timer2')[0];
 
 
 var main_connect = document.getElementById('clickconnect');
@@ -35,7 +36,7 @@ var matchfound_no = document.getElementsByClassName('matchfound_no')[0];
 /*
 			TIMER FUNCTIONS          
 								AUTHOR: Darius			*/	
-function startTimer() {
+function startTimer1() {
 	
 	function updateText(input) {
 		var current_count = timer_count1.innerHTML;
@@ -43,31 +44,47 @@ function startTimer() {
 	}
 	
 	setInterval(updateText, 1000);
-	setTimeout(endTimer, 10000);
+	setTimeout(endTimer1, 15000);
 }
 
-function endTimer() {
+function endTimer1() {
 	drawpic1.classList.add('hidden');
+	times_up.classList.remove('hidden');
+	setTimeout(timesup_to_loading, 2000);
+}
+
+function startTimer2() {
+	
+	function updateText(input) {
+		var current_count = timer_count2.innerHTML;
+		timer_count2.innerHTML = current_count - 1;
+	}
+	
+	setInterval(updateText, 1000);
+	setTimeout(endTimer2, 10000);
+}
+
+function endTimer2() {
+	drawpic2.classList.add('hidden');
 	times_up.classList.remove('hidden');
 	setTimeout(timesup_to_loading, 2000);
 }
 
 
 
-
-
 /*
-			FLOW FUNCTIONS
-								AUTHOR: Darius			*/
+			FLOW FUNCTIONS          
+								AUTHOR: Darius			*/											
 function main_to_drawpic1() {
     main.classList.add('hidden');
     drawpic1.classList.remove('hidden');
-	startTimer();
+	startTimer1();
 }
 
 function drawpic1_to_loading() {
 	drawpic1.classList.add('hidden');
 	loading.classList.remove('hidden');
+	loading_screen_control();
 }
 
 function loading_to_matchfound() {
@@ -78,11 +95,13 @@ function loading_to_matchfound() {
 function matchfound_to_loading() {
 	match_found.classList.add('hidden');
 	loading.classList.remove('hidden');
+	loading_screen_control();
 }
 
 function matchfound_to_drawpic2() {
 	match_found.classList.add('hidden');
 	drawpic2.classList.remove('hidden');
+	startTimer2();
 }
 
 function drawpic2_to_askchatmodal() {
@@ -93,6 +112,7 @@ function drawpic2_to_askchatmodal() {
 function askchatmodal_to_loading() {
 	askchatmodal.classList.add('hidden');
 	loading.classList.remove('hidden');
+	loading_screen_control();
 }
 
 function askchatmodal_to_chat() {
@@ -103,6 +123,15 @@ function askchatmodal_to_chat() {
 function timesup_to_loading() {
 	times_up.classList.add('hidden');
 	loading.classList.remove('hidden');
+	loading_screen_control();
+}
+
+
+
+
+
+function loading_screen_control() {
+	setTimeout(loading_to_matchfound, 5000);
 }
 
 
@@ -114,10 +143,10 @@ function timesup_to_loading() {
 
 
 /*
-			EVENT LISTENERS
+			EVENT LISTENERS          
 								AUTHOR: TEAM			*/
 main_connect.addEventListener('click', main_to_drawpic1);
-askchatmodal_yes.addEventListener('click', askchatmodal_chat);
-askchatmodal_no.addEventListener('click', askchatmodal_loading);
+askchatmodal_yes.addEventListener('click', askchatmodal_to_chat);
+askchatmodal_no.addEventListener('click', askchatmodal_to_loading);
 matchfound_yes.addEventListener('click', matchfound_to_drawpic2);
 matchfound_no.addEventListener('click', matchfound_to_loading);
